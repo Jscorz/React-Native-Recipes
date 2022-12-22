655024526168df4ab91332263ad811e1904e78f9
Justin Scorzafava <jkscorzafava@gmail.com>
2022-12-21 21:06:59 -0700

    added javascript object MealItemProps and used spread operator to props drilling it into MealItem component

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,12 +1,17 @@
 import { View, Pressable, Text, Image, StyleSheet } from "react-native";
 
-function MealItem({ title, imageUrl }) {
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 	return (
 		<View>
 			<Pressable>
 				<View>
 					<Image source={{ uri: imageUrl }} style={styles.image} />
 					<Text style={styles.title}>{title}</Text>
+				</View>
+				<View>
+					<Text>{duration}m</Text>
+					<Text>{complexity.toUpperCase()}</Text>
+					<Text>{affordability.toUpperCase()}</Text>
 				</View>
 			</Pressable>
 		</View>
