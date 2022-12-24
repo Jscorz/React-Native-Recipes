42c8ec8d67e98a595bea855a988131ae10287030
Justin Scorzafava <jkscorzafava@gmail.com>
2022-12-23 21:03:57 -0700

    finished styling with minor differences for android and iphone

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,22 @@
-import { View, Pressable, Text, Image, StyleSheet } from "react-native";
+import { View, Pressable, Text, Image, StyleSheet, Platform } from "react-native";
 
 function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 	return (
-		<View>
-			<Pressable>
-				<View>
-					<Image source={{ uri: imageUrl }} style={styles.image} />
-					<Text style={styles.title}>{title}</Text>
-				</View>
-				<View>
-					<Text>{duration}m</Text>
-					<Text>{complexity.toUpperCase()}</Text>
-					<Text>{affordability.toUpperCase()}</Text>
+		<View style={styles.mealIteam}>
+			<Pressable
+				android_ripple={{ color: "#ccc" }}
+				style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+			>
+				<View style={styles.innerContainer}>
+					<View>
+						<Image source={{ uri: imageUrl }} style={styles.image} />
+						<Text style={styles.title}>{title}</Text>
+					</View>
+					<View style={styles.details}>
+						<Text style={styles.detailItem}>{duration}m</Text>
+						<Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+						<Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+					</View>
 				</View>
 			</Pressable>
 		</View>
@@ -21,6 +26,21 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 export default MealItem;
 
 const styles = StyleSheet.create({
+	mealIteam: {
+		margin: 16,
+		borderRadius: 8,
+		overflow: Platform.OS === "android" ? "hidden" : "visible",
+		backgroundColor: "white",
+		elevation: 4,
+		shadowColor: "black",
+		shadowOpacity: 0.35,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 16,
+	},
+	innerContainer: {
+		borderRadius: 8,
+		overflow: "hidden",
+	},
 	image: {
 		width: "100%",
 		height: 200,
@@ -29,5 +49,19 @@ const styles = StyleSheet.create({
 		fontWeight: "bold",
 		textAlign: "center",
 		fontSize: 18,
+		margin: 8,
+	},
+	details: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 8,
+	},
+	detailItem: {
+		marginHorizontal: 4,
+		fontSize: 12,
+	},
+	buttonPressed: {
+		opacity: 0.5,
 	},
 });
