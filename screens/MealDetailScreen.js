e59a5ef278e4cbb10d843651d0eb5070af0c3ca8
Jscorz <jkscorzafava@gmail.com>
2023-01-02 13:21:23 -0700

    added array around styles to input second styles parameter for MealDetails over MealDetailsScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,29 +1,66 @@
-import { View, Text, Image } from "react-native";
-import MealDetails from '../components/MealDetails';
+import { View, Text, Image, StyleSheet } from "react-native";
+import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
 
 function MealDetailScreen({ route }) {
 	const mealId = route.params.mealId;
 
-	const selectedMeal = MEALS.find((meal) => meal.id === mealId
-    );
+	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
 	return (
 		<View>
-			<Image source={{uri: selectedMeal.imageUrl}}/>
-			<Text>{selectedMeal.title}</Text>
-			<MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability}/>
-			<Text>Ingredients</Text>
-            {selectedMeal.ingredients.map(ingredient => {
-                <Text key={ingredient}>{ingredient}</Text>
-            })}
-			<Text>Steps</Text>
-                        {selectedMeal.ingredients.map(step => {
-                <Text key={step}>{step}</Text>
-            })}
+			<Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+			<Text style={styles.title}>{selectedMeal.title}</Text>
+			<MealDetails
+				duration={selectedMeal.duration}
+				complexity={selectedMeal.complexity}
+				affordability={selectedMeal.affordability}
+				textStyle={styles.detailText}
+			/>
+			<View style={styles.subtitleContainer}>
+				<Text style={styles.subtitle}>Ingredients</Text>
+			</View>
+			{selectedMeal.ingredients.map((ingredient) => {
+				<Text key={ingredient}>{ingredient}</Text>;
+			})}
+			<View style={styles.subtitleContainer}>
+				<Text style={styles.subtitle}>Steps</Text>
+			</View>
+			{selectedMeal.ingredients.map((step) => {
+				<Text key={step}>{step}</Text>;
+			})}
 		</View>
 	);
 }
 
 export default MealDetailScreen;
-+
+
+const styles = StyleSheet.create({
+	image: {
+		width: "100%",
+		height: 350,
+	},
+	title: {
+		fontWeight: "bold",
+		fontSize: 24,
+		margin: 8,
+		textAlign: "center",
+		color: "white",
+	},
+	detailText: {
+		color: "white",
+	},
+	subtitle: {
+		color: "white",
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	subtitleContainer: {
+		marginHorizontal: 24,
+		marginVertical: 4,
+		padding: 6,
+		borderBottomColor: "white",
+		borderBottomWidth: 2,
+	},
+});
